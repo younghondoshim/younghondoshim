ccab610c2f476ba45879e0dea6c92c50a7580002
김유한 <yuhan6653@gmail.com>
2024-07-21 14:41:23 +0900

    0721-1

--- src/components/body/MainWorks.js
+++ src/components/body/MainWorks.js
@@ -1,14 +1,86 @@
 import React from "react";
 import "../../assets/styles/body/MainWorks.css";
-import InstagramFeed from "../../services/instagram";
+import arrowRight from "../../assets/images/arrowRight.svg";
 
 function MainWorks() {
   return (
     <div className="mainWorksWrap">
       <div></div>
       <div className="mainWorksBox">
-        <div className="mainWorksTitle">NEWS</div>
-        <InstagramFeed />
+        <div className="mainWorksTitle">DISCOGRAPHY</div>
+        <div className="mainWorksGrid">
+          <div className="mainWorksGridBorder mainWorksGridText">
+            <div className="mainWorksGridUpperText">
+              <div>영혼도심의</div>
+              <div>디스코그래피</div>
+
+              <div>
+                <a href="https://younghondoshim.bandcamp.com/">
+                  더보기
+                  <img
+                    src={arrowRight}
+                    alt="arrowRight"
+                    className="mainWorksarrowRight"
+                  />
+                </a>
+              </div>
+            </div>
+            <div className="mainWorksGridLowerText">Produce By RENE</div>
+          </div>
+          <div className="mainWorksGridBorder">
+            <iframe
+              style={{
+                border: 0,
+                width: 303,
+                height: 423,
+                fontFamily: "IBMPlexSansKr",
+              }}
+              src="https://bandcamp.com/EmbeddedPlayer/album=1187629348/size=large/bgcol=ffffff/linkcol=04260c/tracklist=false/transparent=true/"
+              seamless
+              title="Skyland"
+            >
+              <a href="https://younghondoshim.bandcamp.com/album/skyland">
+                skyland by RENE
+              </a>
+            </iframe>
+          </div>
+          <div className="mainWorksGridBorder">
+            <iframe
+              style={{ border: 0, width: 303, height: 423 }}
+              src="https://bandcamp.com/EmbeddedPlayer/album=3681298631/size=large/bgcol=ffffff/linkcol=04260c/tracklist=false/transparent=true/"
+              seamless
+              title="GEMS"
+            >
+              <a href="https://younghondoshim.bandcamp.com/album/gems">
+                GEMS by IC KID, RENE
+              </a>
+            </iframe>
+          </div>
+          <div className="mainWorksGridBorder">
+            <iframe
+              style={{ border: 0, width: 303, height: 423 }}
+              src="https://bandcamp.com/EmbeddedPlayer/album=2320034669/size=large/bgcol=ffffff/linkcol=04260c/tracklist=false/transparent=true/"
+              seamless
+              title="Mystery"
+            >
+              <a href="https://younghondoshim.bandcamp.com/album/mystery">
+                Mystery by RENE
+              </a>
+            </iframe>
+          </div>
+          <div className="mainWorksGridBorder">
+            <iframe
+              style={{ border: 0, width: 303, height: 423 }}
+              src="https://bandcamp.com/EmbeddedPlayer/album=389456900/size=large/bgcol=ffffff/linkcol=04260c/tracklist=false/track=2741013736/transparent=true/"
+              seamless
+              title="Entropy"
+            >
+              <a href="https://younghondoshim.bandcamp.com/album/entropy">
+                entropy by KWAII + RENE
+              </a>
+            </iframe>
+          </div>
+        </div>
         <div className="mainworksGrid"></div>
       </div>
     </div>
