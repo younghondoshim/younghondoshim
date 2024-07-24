59f104ed464ee244f8b3f0d693ebfdb653b5ebcc
김유한 <yuhan6653@gmail.com>
2024-07-24 18:39:41 +0900

    0724-2

--- src/components/animation/UseInView.js
+++ src/components/animation/UseInView.js
@@ -12,16 +12,17 @@ const useInView = (options) => {
       }
     }, options);
 
-    if (ref.current) {
-      observer.observe(ref.current);
+    const currentRef = ref.current;
+    if (currentRef) {
+      observer.observe(currentRef);
     }
 
     return () => {
-      if (ref.current) {
-        observer.unobserve(ref.current);
+      if (currentRef) {
+        observer.unobserve(currentRef);
       }
     };
-  }, [ref, options]);
+  }, [options]);
 
   return [ref, inView];
 };
